baf17facdde8367433f8296afb9657c17457923c
LironSaedi <lionsaedi@gmail.com>
2018-09-07 17:30:28 -0700

    Inital Commit

---
+++ warmUp27/warmUp27/Program.cs
@@ -0,0 +1,53 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace warmUp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ranGen = 0;
+            int[] array = new int[8];
+
+            Random ran = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                ranGen = ran.Next(0, 10);
+
+                array[i] = ranGen;
+            }
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{array[i]}");
+            }
+            Console.ReadKey();
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                array[i] = array[i] + array[i + 1];
+
+
+            }
+
+
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < array[i + 1])
+                {
+                    array[i] = array[i + 1];
+                }
+
+            }
+
+
+        }
+    }
+}
